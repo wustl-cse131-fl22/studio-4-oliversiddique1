301cb3f616cd25fb99f6333e583a5b226607e988
fareeha <fareeha@localhost>
2022-10-13 14:12:51 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,15 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	StdDraw.setPenColor(Color.BLACK);
+	StdDraw.rectangle(.5, .5, .4, .2);
+	StdDraw.setPenColor(Color.BLUE);
+	StdDraw.line(0.2, 0.6, 0.8, 0.6);
+	StdDraw.line(0.2, 0.4, 0.8, 0.4);
+	StdDraw.setPenColor(Color.RED);
+	StdDraw.line(0.2, 0.35, 0.8, 0.35);
+	StdDraw.line(0.2, 0.65, 0.8, 0.65);
+	StdDraw.setPenColor(Color.YELLOW);
+	StdDraw.filledRectangle(.5, .5, .2, .05);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,76 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		
+		int redComponent = in.nextInt();
+		
+		int greenComponent = in.nextInt();
+		
+		int blueComponent = in.nextInt();
+		
+		boolean isFilled = in.nextBoolean();
+		
+		
+		if (shapeType.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (isFilled == true) {
+			StdDraw.filledRectangle(x, y, halfWidth, halfHeight);}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+			}
+		
+		
+		if (shapeType.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (isFilled == true) {
+			StdDraw.filledEllipse(x, y, halfWidth, halfHeight);}
+			else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+				
+			}
+//		if (shapeType == "triangle") {
+//			double x1 = in.nextDouble();
+//			double y1 = in.nextDouble();
+//			double x2 = in.nextDouble();
+//			double y2 = in.nextDouble();
+//			double x3 = in.nextDouble();
+//			double y3 = in.nextDouble();
+//			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+//			if (isFilled == true) {
+//			StdDraw.filledTriangle(x1, y1, x2, y2, x3, y3);}
+//			else {
+//				StdDraw.Triangle(x1, y1, x2, y2, x3, y3);
+//				StdDraw.
+//			}
+//				
+//		
+//		}
+		
+		double parameterOne = in.nextDouble();
+		
+		double parameterTwo = in.nextDouble();
+		
+		double parameterThree = in.nextDouble();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		
 		
 	}
 }
